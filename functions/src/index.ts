924c591edff80220eca67d4e0c23ff0be9f99c66
Bing Yang <bingyang@nanometrics.ca>
2020-06-03 15:57:45 -0400

    update functions/package.json and index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,21 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+admin.initializeApp();
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
